b146493d7e88407f622b0ba9944d8812c85c2d12
Yurick <yurick.hauschild@gmail.com>
2019-02-28 23:27:18 -0300

    🎉  Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+export default function potato(): number {
+  return 3
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+describe("ouxe", () => {
+  it("makes sense", () => {
+    expect(true).toBe(true);
+  });
+});

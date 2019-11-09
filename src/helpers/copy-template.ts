356cd56ada61b054ba177e76a622e626ef680286
Yurick <yurick.hauschild@gmail.com>
2019-11-09 21:18:29 +0100

    🐛 Fix source of templates

--- src/helpers/copy-template.ts
+++ src/helpers/copy-template.ts
@@ -2,5 +2,5 @@ import * as fs from 'fs-extra'
 import * as path from 'path'
 
 export default function copyTemplate(name: string): void {
-  fs.copySync(path.join(__dirname, '..', 'templates', name), `./${name}`)
+  fs.copySync(path.join(__dirname, '..', '..', 'templates', name), `./${name}`)
 }

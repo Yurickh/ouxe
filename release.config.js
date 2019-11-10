d834c1ffabfbed6c24b2832ebb849b793698e30a
Yurick <yurick.hauschild@gmail.com>
2019-11-10 11:01:34 +0100

    📦 Configure semantic release

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    'semantic-release-gitmoji',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'lib/ouxe.js', label: 'Standard distribution' },
+          { path: 'lib/ouxe.mjs', label: 'ES6 modules distribution' },
+          { path: 'lib/ouxe.modern.js', label: 'Modern JS distribution' },
+          { path: 'lib/ouxe.umd.js', label: 'UMD distribution' },
+        ],
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: '🔖 ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
